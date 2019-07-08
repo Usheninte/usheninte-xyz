31d07451371542b70104ec5b2b0864eb56a4d3ab
Usheninte <jrandangana@gmail.com>
2019-07-08 08:49:58 +0100

    building app.js

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTO_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: "",
+  messagingSenderId: process.env.MSG_SEND_ID,
+  appId: process.env.APP_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
